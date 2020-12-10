d343e25d25d1adac1633174bc4ddbd4763dc3947
service it <service-it-01@foto.com>
2020-12-10 12:34:32 +0100

    repo add

---
+++ DAO/Repository/RepositoryBase.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAO.Repository
+{
+    public abstract class RepositoryBase
+    {
+        // protected string connectionString = @"Data Source=DESKTOP-MHV8JRA\SQLEXPRESS;Initial Catalog=heroDb;Integrated Security=True;";
+        protected string connectionString = @"Data Source=192.168.1.119;Initial Catalog=heroDb;Integrated Security=false;User Id=heroDb;Password=abc123;";
+        // @"Data Source = 192.168.1.119; Initial Catalog = heroDb; User ID = sa; Password=********;Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;"
+    }
+}
